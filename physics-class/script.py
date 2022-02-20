7951e8af1271ed25852847a58de501f058fac64e
Gayatri R <gayatrirajgor@gmail.com>
2022-02-20 19:20:19 +0000

    created f to c function

---
+++ physics-class/script.py
@@ -0,0 +1,15 @@
+# Uncomment this when you reach the "Use the Force" section
+# train_mass = 22680
+# train_acceleration = 10
+# train_distance = 100
+# bomb_mass = 1
+
+
+# Write your code below: 
+
+def f_to_c(f_temp):
+    return (f_temp - 32) * 5/9
+    
+
+f100_in_celsius = f_to_c(100)
+print(f100_in_celsius)
